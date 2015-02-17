4c95ce919b22707b6c13134f6398d7df9e71cc9a
Richard Lister <rlister+gh@gmail.com>
2015-02-17 16:08:13 -0500

    use default confdir of /etc/confd

--- nginx-confd/app.sh
+++ nginx-confd/app.sh
@@ -7,14 +7,14 @@ set -eo pipefail
 export ETCD=${ETCD:-http://172.17.42.1:4001}
 
 ## wait until we can make initial nginx config
-until ./confd -verbose -onetime -node $ETCD -confdir /app ; do
+until ./confd -verbose -onetime -node $ETCD ; do
   echo "confd waiting to create initial nginx config"
   sleep 5
 done
 
 ## run confd to poll etcd for changes
 echo "confd polling etcd ..."
-./confd -verbose -interval 10 -node $ETCD -confdir /app &
+./confd -verbose -interval 10 -node $ETCD &
 
 ## run nginx in foreground
 nginx -g 'daemon off;'
